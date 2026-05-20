bb3c447e3bbf55035837159b25299e296bcffa7b
nhi-kk <khanhikieu196@gmail.com>
2026-05-20 22:51:35 +0700

    server js

--- server.js
+++ server.js
@@ -1,135 +1,54 @@
-const express=require("express")
-const cors=require("cors")
-const multer=require("multer")
-const path=require("path")
+const express = require("express")
+const cors = require("cors")
+const multer = require("multer")
+const path = require("path")
 require("dotenv").config()
 
-const app=express()
+const app = express()
 
+// Bật CORS cho phép GitHub Pages truy cập dữ liệu công khai
 app.use(cors())
 
 app.use(express.json({
-limit:"50mb"
+    limit: "50mb"
 }))
 
 app.use(express.urlencoded({
-extended:true,
-limit:"50mb"
+    extended: true,
+    limit: "50mb"
 }))
 
-app.use(
-express.static("public")
-)
+app.use(express.static("public"))
+app.use("/uploads", express.static("uploads"))
 
-app.use(
-"/uploads",
-express.static("uploads")
-)
-
-const storage=
-
-multer.diskStorage({
-
-destination:(req,file,cb)=>{
-
-cb(
-null,
-"./uploads"
-)
-
-},
-
-filename:(req,file,cb)=>{
-
-const ext=
-
-path.extname(
-file.originalname
-)
-
-const fileName=
-
-Date.now()+ext
-
-cb(
-null,
-fileName
-)
-
-}
-
+// Cấu hình lưu trữ ảnh upload bằng Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        const fileName = Date.now() + ext
+        cb(null, fileName)
+    }
 })
 
-const upload=
+const upload = multer({ storage })
 
-multer({
-
-storage
-
+app.post("/api/upload", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ success: false })
+    }
+    res.json({
+        success: true,
+        url: "/uploads/" + req.file.filename
+    })
 })
 
-app.post(
+app.use("/api/menu", require("./server/routes/menu"))
+app.use("/api/orders", require("./server/routes/orders"))
 
-"https://drink-order-backend.onrender.com/api/upload",
-
-upload.single(
-"image"
-),
-
-(req,res)=>{
-
-if(
-!req.file
-){
-
-return res
-.status(400)
-.json({
-
-success:false
-
+// Chạy Server Backend
+app.listen(process.env.PORT, () => {
+    console.log("SERVER IS RUNNING ON PORT:", process.env.PORT)
 })
-
-}
-
-res.json({
-
-success:true,
-
-url:
-
-"/uploads/"+
-req.file.filename
-
-})
-
-}
-
-)
-
-app.use(
-"https://drink-order-backend.onrender.com/api/menu",
-require("./server/routes/menu")
-)
-
-app.use(
-"https://drink-order-backend.onrender.com/api/orders",
-require("./server/routes/orders")
-)
-
-app.listen(
-
-process.env.PORT,
-
-()=>{
-
-console.log(
-
-"RUNNING",
-
-process.env.PORT
-
-)
-
-}
-)
